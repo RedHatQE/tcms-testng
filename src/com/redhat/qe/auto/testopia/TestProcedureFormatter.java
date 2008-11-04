02567af0076d228d8ec6bab27271b57833b69cce
jweiss <jweiss@ad8b2fa6-1fe3-0310-8810-ca5544d99bb5>
2008-11-04 14:24:22 +0000

    IN PROGRESS - issue SAND-17: Logger that only logs actions (and higher), in order to upload them to Testopia.  Moved log classes into new package. http://jira.rhq-project.org/browse/SAND-17
    git-svn-id: svn://svn.rhndev.redhat.com:55555/svn/rhn-svn/trunk/qa/automation/webui-framework@178414 ad8b2fa6-1fe3-0310-8810-ca5544d99bb5

---
+++ src/com/redhat/qe/auto/testopia/TestProcedureFormatter.java
@@ -0,0 +1,20 @@
+package com.redhat.qe.auto.testopia;
+
+import java.util.logging.LogRecord;
+
+import com.redhat.qe.auto.selenium.LogFormatter;
+import com.redhat.qe.auto.selenium.MyLevel;
+
+public class TestProcedureFormatter extends LogFormatter {
+
+	@Override
+	public String format(LogRecord record) {
+		if (!record.getLevel().equals(MyLevel.ACTION)) return super.format(record);
+		else {
+			String throwable = "";
+			if (record.getThrown() != null) throwable = throwableToString(record.getThrown())  + "\n";
+			return record.getMessage() + "\n" + throwable;
+		}
+	}
+	
+}
